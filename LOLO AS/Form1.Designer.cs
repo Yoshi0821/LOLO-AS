867acf7216f3122c0717c9afa91b71dbe20051ce
Bryan Alberto Vargas Reyes <bryanbeto1234@gmail.com>
2020-11-11 21:44:37 -0600

    bryan 16

--- LOLO AS/Form1.Designer.cs
+++ LOLO AS/Form1.Designer.cs
@@ -31,8 +31,8 @@ namespace LOLO_AS
         {
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -43,7 +43,7 @@ namespace LOLO_AS
             this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.label1.Location = new System.Drawing.Point(12, 64);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(126, 37);
+            this.label1.Size = new System.Drawing.Size(133, 39);
             this.label1.TabIndex = 0;
             this.label1.Text = "Prueba";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -59,16 +59,6 @@ namespace LOLO_AS
             this.panel1.Size = new System.Drawing.Size(776, 239);
             this.panel1.TabIndex = 1;
             // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.button1.Location = new System.Drawing.Point(570, 68);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(180, 126);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = false;
-            // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
@@ -78,6 +68,16 @@ namespace LOLO_AS
             this.button2.TabIndex = 3;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(571, 68);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 126);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
