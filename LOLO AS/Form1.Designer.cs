520401e50d37b1ced36b4337aa81b5c4a4108c04
Bryan Alberto Vargas Reyes <bryanbeto1234@gmail.com>
2020-11-11 21:39:17 -0600

    Bryan 15

--- LOLO AS/Form1.Designer.cs
+++ LOLO AS/Form1.Designer.cs
@@ -32,6 +32,7 @@ namespace LOLO_AS
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -50,6 +51,7 @@ namespace LOLO_AS
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.button1);
             this.panel1.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.panel1.Location = new System.Drawing.Point(12, 199);
@@ -66,6 +68,16 @@ namespace LOLO_AS
             this.button1.TabIndex = 2;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(143, 68);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(180, 126);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
@@ -87,6 +99,7 @@ namespace LOLO_AS
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
