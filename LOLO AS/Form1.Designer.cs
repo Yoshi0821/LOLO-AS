ba06551838aeccbd3aab499b986536156f7685cc
Yoshi0821 <josias0821@gmail.com>
2020-11-11 20:55:53 -0600

    Josias 4

--- LOLO AS/Form1.Designer.cs
+++ LOLO AS/Form1.Designer.cs
@@ -40,9 +40,9 @@ namespace LOLO_AS
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.03478F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label1.Location = new System.Drawing.Point(309, 115);
+            this.label1.Location = new System.Drawing.Point(338, 67);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(133, 39);
+            this.label1.Size = new System.Drawing.Size(126, 37);
             this.label1.TabIndex = 0;
             this.label1.Text = "Prueba";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -51,7 +51,6 @@ namespace LOLO_AS
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.panel1.Controls.Add(this.button1);
-            this.panel1.Controls.Add(this.label1);
             this.panel1.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.panel1.Location = new System.Drawing.Point(12, 199);
             this.panel1.Name = "panel1";
@@ -74,11 +73,12 @@ namespace LOLO_AS
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
