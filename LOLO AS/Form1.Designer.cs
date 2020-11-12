7e697bcec3f439c5b5ef773a827c7d1aa351b27e
Bryan Alberto Vargas Reyes <bryanbeto1234@gmail.com>
2020-11-11 20:54:17 -0600

    Bryan 4

--- LOLO AS/Form1.Designer.cs
+++ LOLO AS/Form1.Designer.cs
@@ -31,6 +31,8 @@ namespace LOLO_AS
         {
             this.label1 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -38,9 +40,9 @@ namespace LOLO_AS
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.03478F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label1.Location = new System.Drawing.Point(330, 50);
+            this.label1.Location = new System.Drawing.Point(309, 115);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(126, 37);
+            this.label1.Size = new System.Drawing.Size(133, 39);
             this.label1.TabIndex = 0;
             this.label1.Text = "Prueba";
             this.label1.Click += new System.EventHandler(this.label1_Click);
@@ -48,23 +50,35 @@ namespace LOLO_AS
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label1);
             this.panel1.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.panel1.Location = new System.Drawing.Point(12, 199);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(776, 239);
             this.panel1.TabIndex = 1;
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(573, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 126);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -72,6 +86,7 @@ namespace LOLO_AS
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
